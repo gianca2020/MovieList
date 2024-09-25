fce1c4809f20e5d62060b24f82acb8ba3480aa95
gianca2020 <giancarloforero@gmail.com>
2024-09-25 01:14:07 -0400

    i have a quiz tommorow

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import Row from 'react-bootstrap/Row';
 import CustomNavbar from "./components/customNavbar";
 import MovieCard from "./components/MovieCard";
+import 'C:/Users/gianc/MovieList/src/App.css';
+// import image1 from '.images/lalaLand.jpg';
 
 
 function App() {
 
   const movies = [
-    {name: "Perfect Blue", director: "Satoshi Kon", year: 1997},
+    {imageUrl: 'https://m.media-amazon.com/images/M/MV5BYjkyZjY1OWItNjYyNC00ZjlhLTgwOTAtMGNiYTFmYTNmZDI3XkEyXkFqcGc@._V1_.jpg', name: "Perfect Blue", director: "Satoshi Kon", year: 1997},
     {name: "lalaLand", director: "Damien Schazelle", year: 2015},
     {name: "The Muppets", director: "James Bobin", year: 2011}
   ]
@@ -24,7 +26,9 @@ function App() {
       <Container fluid>
         <Row>
           <Col>
-            <MovieCard name={movies[0].name}
+            <MovieCard 
+              imageUrl={movies[0].imageUrl}
+              name={movies[0].name}
               director={movies[0].director}
               year={movies[0].year}/>
           </Col>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Card } from 'react-bootstrap';
 
+
+
 const MovieCard = (props) => {
     return(
         <Card>
               <Card.Img
                 variant="top"
-                src="https://placeholder.co/200x200"
-                alt="Placeholder image"
+                /*change this src either in card title or here*/ 
+                // src="https://placeholder.co/200x200"
+                 src={props.imageUrl}
+                alt= "haha, try again loser"
               />
               <Card.Body>
-                <Card.Title>{`Movie: ${props.name}`}</Card.Title> 
+                <Card.Title>{`Movie: ${props.imageUrl}`}</Card.Title> 
                 <Card.Text>
                   <h4>{`Movie: ${props.director}`}</h4>
                   <h5>{`Movie: ${props.year}`}  </h5>
